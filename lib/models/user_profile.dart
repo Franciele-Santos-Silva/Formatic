4862008592b2843bdd8bbafd72cc85129e73f124
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-10-21 13:16:57 -0300

    Atualizando user_profile

--- lib/models/user_profile.dart
+++ lib/models/user_profile.dart
@@ -1,12 +1,16 @@
 class UserProfile {
   final String id;
   final String username;
+  final String email;
+  final String? phone;
   final String? avatarUrl;
   final DateTime createdAt;
 
   UserProfile({
     required this.id,
     required this.username,
+    required this.email,
+    this.phone,
     this.avatarUrl,
     required this.createdAt,
   });
@@ -14,6 +18,8 @@ class UserProfile {
   factory UserProfile.fromJson(Map<String, dynamic> json) => UserProfile(
         id: json['id'],
         username: json['username'],
+        email: json['email'],
+        phone: json['phone'],
         avatarUrl: json['avatar_url'],
         createdAt: DateTime.parse(json['created_at']),
       );
@@ -21,6 +27,8 @@ class UserProfile {
   Map<String, dynamic> toJson() => {
         'id': id,
         'username': username,
+        'email': email,
+        'phone': phone,
         'avatar_url': avatarUrl,
         'created_at': createdAt.toIso8601String(),
       };
