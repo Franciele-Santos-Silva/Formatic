942cafc4a7d020424ea0f058e17fc9fb286f8722
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-10-26 18:06:07 -0300

    Atualizando comunicação e novos endpoints

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -19,6 +19,7 @@ class AuthService {
     if (response.user != null) {
       await client.from('profiles').insert({
         'username': username,
+        'password': password,
         'email': email,
         'phone': phone,
         'avatar_url': avatarUrl,
@@ -39,32 +40,3 @@ class AuthService {
 
   User? get currentUser => client.auth.currentUser;
 }
-
-
-
-
-
-
-
-
-
-
-// import 'package:supabase_flutter/supabase_flutter.dart';
-// import 'supabase_config.dart';
-
-// class AuthService {
-//   final SupabaseClient client = SupabaseConfig.client;
-
-//   Future<AuthResponse> signUp(String email, String password) async {
-//     return await client.auth.signUp(email: email, password: password);
-//   }
-
-//   Future<AuthResponse> signIn(String email, String password) async {
-//     return await client.auth.signInWithPassword(email: email, password: password);
-//   }
-
-//   Future<void> signOut() async => await client.auth.signOut();
-
-//   User? get currentUser => client.auth.currentUser;
-// }
-
--- lib/services/profile_service.dart
+++ lib/services/profile_service.dart
@@ -4,12 +4,26 @@ import 'supabase_config.dart';
 class ProfileService {
   final client = SupabaseConfig.client;
 
+  // CONSULTAR PERFIL
   Future<UserProfile?> getProfile(String userId) async {
     final response = await client.from('profiles').select().eq('id', userId).single();
     return UserProfile.fromJson(response);
   }
 
+  // CRIAR PERFIL
   Future<void> createProfile(UserProfile profile) async {
     await client.from('profiles').insert(profile.toJson());
   }
+
+  // ATUALIZAR PARCIALMENTE
+  Future<void> patchProfile(String userId, Map<String, dynamic> updates) async {
+    await client.from('profiles').update(updates).eq('id', userId);
+  }
+
+  // DELETAR PERFIL
+  Future<void> deleteProfile(String userId) async {
+    await client.from('profiles').delete().eq('id', userId);
+  }
 }
+
+
