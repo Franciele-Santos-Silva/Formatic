3b12868fc283239e2fee20f69447559314bd4bc9
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-11-06 17:38:26 -0300

    Organizando models e services

--- lib/services/study_service.dart
+++ lib/services/study_service.dart
@@ -5,15 +5,6 @@ import 'supabase_config.dart';
 class StudyService {
   final client = SupabaseConfig.client;
 
-  Future<List<StudySession>> getUserSessions() async {
-    final response = await client
-        .from('study_sessions')
-        .select()
-        .order('created_at', ascending: false);
-
-    return (response as List).map((e) => StudySession.fromJson(e)).toList();
-  }
-
   Future<StudySession> startSession(StudySession session) async {
     final response = await client
         .from('study_sessions')
@@ -35,13 +26,6 @@ class StudyService {
     return StudySession.fromJson(response);
   }
 
-  Future<void> deleteSession(String sessionId) async {
-    await client
-        .from('study_sessions')
-        .delete()
-        .eq('id', sessionId);
-  }
-
   Future<StudyMetrics?> getTodayMetrics() async {
     final today = DateTime.now().toIso8601String().split('T')[0];
     
@@ -52,54 +36,5 @@ class StudyService {
         .maybeSingle();
 
     return response != null ? StudyMetrics.fromJson(response) : null;
-  }
-
-  Future<StudyMetrics> updateMetrics(StudyMetrics metrics) async {
-    final response = await client
-        .from('study_metrics')
-        .upsert(metrics.toJson())
-        .select()
-        .single();
-
-    return StudyMetrics.fromJson(response);
-  }
-
-  Future<List<StudyMetrics>> getWeeklyMetrics() async {
-    final weekAgo = DateTime.now().subtract(Duration(days: 7));
-    final formattedDate = weekAgo.toIso8601String().split('T')[0];
-
-    final response = await client
-        .from('study_metrics')
-        .select()
-        .gte('date', formattedDate)
-        .order('date', ascending: true);
-
-    return (response as List).map((e) => StudyMetrics.fromJson(e)).toList();
-  }
-
-  Future<Map<String, dynamic>> getTodayStats() async {
-    final today = DateTime.now().toIso8601String().split('T')[0];
-    
-    final response = await client
-        .from('study_sessions')
-        .select('duration_minutes, session_type')
-        .eq('completed', true)
-        .gte('created_at', today);
-
-    int totalMinutes = 0;
-    int pomodoroCount = 0;
-
-    for (var session in response) {
-      totalMinutes += session['duration_minutes'] as int;
-      if (session['session_type'].toString().contains('pomodoro')) {
-        pomodoroCount++;
-      }
-    }
-
-    return {
-      'totalMinutes': totalMinutes,
-      'pomodoroSessions': pomodoroCount,
-      'sessionCount': response.length,
-    };
   }
 }
